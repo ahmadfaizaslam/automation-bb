bffa2014369739d528459f3637509cd2213e03ad
ahmadfaizaslam <faizaslam1996@gmail.com>
2021-04-02 08:14:08 +0800

    test

---
+++ x.py
@@ -0,0 +1,8 @@
+
+# masterbb3.loc[masterbb3['PL IPL IPL R&R NPL'] == 'IPL', 'Risk Cat GCMC'] = 'IPL'
+# masterbb3.loc[masterbb3['PL IPL IPL R&R NPL'] == 'IPL R&R', 'Risk Cat GCMC'] = 'IPL R&R'
+# masterbb3.loc[masterbb3['PL IPL IPL R&R NPL'] == 'NPL', 'Risk Cat GCMC'] = 'NPL'
+
+
+for key in[('IPL','IPL R&R','NPL')]:
+    
